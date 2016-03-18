1fc05d4112de02ea7b8e60df4a3b3bd004405069
chris <chrisberry321@yahoo.com>
2016-03-18 15:45:23 -0700

    using the api to generate information

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,1 +1,15 @@
+var AccountName = require('../js/github.js').AccountName;
+var Discription = require('../js/github.js').Discription;
+var apiKey = require('./../.env').apiKey;
 
+$(document).ready(function() {
+  $('#userSearch').submit(function(event) {
+    event.preventDefault();
+    var userName = $('#userName').val();
+    $.get('https://api.github.com/users/chrisberry321?access_token=' + apiKey).then(function(response){
+    console.log(response);
+    }).fail(function(error){
+    console.log(error.responseJSON.message);
+   });
+   });
+ });
